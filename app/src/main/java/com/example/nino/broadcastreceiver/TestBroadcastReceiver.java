9eea76518ae3a7b60e330037bfc7a8537aef75a4
Nino <nino2armando@yahoo.com>
2014-08-18 15:29:47 -0700

    first draft

--- app/src/main/java/com/example/nino/broadcastreceiver/TestBroadcastReceiver.java
+++ app/src/main/java/com/example/nino/broadcastreceiver/TestBroadcastReceiver.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
  * Created by nino on 18/08/14.
  */
 public class TestBroadcastReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Intent detected", Toast.LENGTH_LONG).show();
