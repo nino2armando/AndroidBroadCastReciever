081ba53ab94507dc2e8bf5cab14303190b0d133a
Nino <nino2armando@yahoo.com>
2014-08-18 13:27:37 -0700

    first draft

---
+++ app/src/main/java/com/example/nino/broadcastreceiver/TestBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.example.nino.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by nino on 18/08/14.
+ */
+public class TestBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Intent detected", Toast.LENGTH_LONG).show();
+    }
+}
